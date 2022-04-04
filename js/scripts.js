71c7386a3efb4f478b510088b6cc2ec38f9936d5
haelime <gnostalgiclife@gmail.com>
2022-04-04 16:07:31 +0900

    deleted js, used AOS to fade in effect

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,3 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-
-const targets = document.querySelectorAll(".fade-in-box");
-const options = { root: null, threshold: 0.1, rootMargin: "-0px" };
-const observer = new IntersectionObserver(function (entries, observer) {
-    entries.forEach((entry) => {
-        const container = entry.target;
-        if (entry.isIntersecting) {
-            container.classList.add("fade-in-box");
-        } else {
-            container.classList.remove("fade-in-box");
-        }
-    });
-}, options);
-
-targets.forEach((target) => {
-    observer.observe(target);
-});
